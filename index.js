a92fa00730f11064c75251aa6b7cd66a2e16a372
Mike Fooks <mikefooks@gmail.com>
2014-08-31 18:19:34 -0400

    improved a little bit - not much

--- index.js
+++ index.js
@@ -13,63 +13,52 @@ function generateFileName(fileName, size) {
     return fileName.replace(imageTest, "_" + size + "$1");
 }
 
-hexo.extend.generator.register("images", function (locals, render, callback) {
+hexo.extend.generator.register("images", function (locals, render, next) {
     var assets = hexo.model("Asset").toArray(),
         baseDir = hexo.base_dir,
         tmpFolder = path.join(baseDir, "imgTmp"),
 
-        imageProperties = assets.filter(function (asset) {
+        // Filter out any asset that isn't an image.
+        imagePaths = assets.filter(function (asset) {
             return imageTest.test(asset._id);
+
+        // ... and build a collection of all the new filenames for
+        // both the temporary and final destinations of the resized
+        // images.
         }).map(function (asset) {
-            var source = path.join(baseDir, asset._id),
+            var tmpPaths = {},
+                destPaths = {},
+                source = path.join(baseDir, asset._id),
                 fileName = path.basename(source),
                 destDir = path.dirname(asset.path);
 
+            sizeKeys.forEach(function (size) {
+                var resizedName = generateFileName(fileName, size),
+                    tmpPath = path.join(tmpFolder, resizedName),
+                    destPath = path.join(destDir, resizedName);
+
+                tmpPaths[size] = tmpPath;
+                destPaths[size] = destPath;
+            });
+
             return {
                 source: source,
-                modified: asset.modified,
-                fileName: fileName,
-                destDir: destDir,
-                getResizedName: function (size) {
-                    return generateFileName(fileName, size);
-                }
+                dest: destPaths,
+                tmp: tmpPaths
             };
         });
 
-    // Create a temporary folder for our newly created images.
-    fs.stat(tmpFolder, function (err) {
-        if (err) {
-            fs.mkdir(tmpFolder, function (err) {
-                if (err) { return callback(err); }
-            }); 
-        }
-    });
+    async.series([
 
-    async.waterfall([
+        // Create a temporary folder for our new images.
         function (callback) {
-            var resizedPaths = imageProperties.map(function (image) {
-                var tmpPaths = {},
-                    destPaths = {};
-                
-                sizeKeys.forEach(function (size) {
-                    var resizedName =  image.getResizedName(size),
-                        tmpPath = path.join(tmpFolder, resizedName),
-                        destPath = path.join(image.destDir, resizedName);
+            fs.mkdir(tmpFolder, callback);
+        },
 
-                    tmpPaths[size] = tmpPath;
-                    destPaths[size] = destPath;
-                });
-                
-                return {
-                    source: image.source,
-                    dest: destPaths,
-                    tmp: tmpPaths
-                };
-            });
-
-            callback(null, resizedPaths);
-        }, function (paths, callback) {
-            async.eachSeries(paths, function (image, callback) {
+        // Generate the new resized images and put them in the
+        // temporary directory.
+        function (callback) {
+            async.eachSeries(imagePaths, function (image, callback) {
                 async.each(sizeKeys, function (size, callback) {
                     gm(image.source)
                         .thumb(
@@ -80,18 +69,21 @@ hexo.extend.generator.register("images", function (locals, render, callback) {
                             callback
                         );
                 }, callback);
-            }, function () {
-                callback(null, paths);
-            });
-        }
-    ], function (err, paths) {
-        async.eachSeries(paths, function (image, callback) {
-            async.each(sizeKeys, function (size, callback) {
-                hexo.route.set(image.dest[size], function (callback) {
-                    callback(null, fs.createReadStream(image.tmp[size]));
-                });
-                callback();
             }, callback);
-        }, callback);
+        },
+
+        // Set the appropriate Hexo route for each image and size.
+        function (callback) {
+            async.eachSeries(imagePaths, function (image, callback) {
+                async.each(sizeKeys, function (size, callback) {
+                    hexo.route.set(image.dest[size], function (fn) {
+                        fn(null, fs.createReadStream(image.tmp[size]));
+                    });
+                    callback();
+                }, callback);
+            }, callback);
+        }
+    ], function (err) {
+        next();
     });
 });
