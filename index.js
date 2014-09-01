030ba1393245a71f1c3439043cbfc3c6a51c83c5
Mike Fooks <mikefooks@gmail.com>
2014-09-01 19:13:09 -0400

    temp folder deleted automatically.

--- index.js
+++ index.js
@@ -1,52 +1,55 @@
 var fs = require("fs"),
     path = require("path"),
     async = require("async"),
-    gm = require("gm");
+    gm = require("gm"),
+    rimraf = require("rimraf");
 
 var imageSizes = hexo.config["responsive_image_sizes"],
     sizeKeys = Object.keys(imageSizes),
     imageTest = new RegExp(/(\.jpg|\.png|\.gif)$/);
 
+var baseDir = hexo.base_dir,
+    tmpFolder = path.join(baseDir, "imgTmp");
 
 // Takes the original image filename and turns it into originalFileName_size.jpg.
 function generateFileName(fileName, size) {
     return fileName.replace(imageTest, "_" + size + "$1");
 }
 
+// Delete the temp folder once generating is done.
+hexo.on("generateAfter", function () {
+    rimraf.sync(tmpFolder);
+});
+
 hexo.extend.generator.register("images", function (locals, render, next) {
-    var assets = hexo.model("Asset").toArray(),
-        baseDir = hexo.base_dir,
-        tmpFolder = path.join(baseDir, "imgTmp"),
+    var assets = hexo.model("Asset");
 
-        // Filter out any asset that isn't an image.
-        imagePaths = assets.filter(function (asset) {
-            return imageTest.test(asset._id);
+    // Filter out any asset that isn't an image...
+    var imagePaths = assets.filter(function (asset) {
+        return imageTest.test(asset._id);
+    // ... and build a collection of all the new filenames for
+    // both the temporary and final destinations of the resized
+    // images.
+    }).map(function (asset) {
+        var tmpPaths = {},
+            destPaths = {},
+            source = path.join(baseDir, asset._id),
+            fileName = path.basename(source),
+            destDir = path.dirname(asset.path);
 
-        // ... and build a collection of all the new filenames for
-        // both the temporary and final destinations of the resized
-        // images.
-        }).map(function (asset) {
-            var tmpPaths = {},
-                destPaths = {},
-                source = path.join(baseDir, asset._id),
-                fileName = path.basename(source),
-                destDir = path.dirname(asset.path);
+        sizeKeys.forEach(function (size) {
+            var resizedName = generateFileName(fileName, size);
 
-            sizeKeys.forEach(function (size) {
-                var resizedName = generateFileName(fileName, size),
-                    tmpPath = path.join(tmpFolder, resizedName),
-                    destPath = path.join(destDir, resizedName);
-
-                tmpPaths[size] = tmpPath;
-                destPaths[size] = destPath;
-            });
-
-            return {
-                source: source,
-                dest: destPaths,
-                tmp: tmpPaths
-            };
+            tmpPaths[size] = path.join(tmpFolder, resizedName);
+            destPaths[size] = path.join(destDir, resizedName);
         });
+
+        return {
+            source: source,
+            dest: destPaths,
+            tmp: tmpPaths
+        };
+    });
 
     async.series([
 
@@ -76,8 +79,9 @@ hexo.extend.generator.register("images", function (locals, render, next) {
         function (callback) {
             async.eachSeries(imagePaths, function (image, callback) {
                 async.each(sizeKeys, function (size, callback) {
+                    var rs = fs.createReadStream(image.tmp[size]);
                     hexo.route.set(image.dest[size], function (fn) {
-                        fn(null, fs.createReadStream(image.tmp[size]));
+                        fn(null, rs);
                     });
                     callback();
                 }, callback);
